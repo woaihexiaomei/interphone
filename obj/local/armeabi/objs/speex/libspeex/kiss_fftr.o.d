51e6f8e230ab6ee159eccf24f8af97fc348b7b6d
wan <784729370@qq.com>
2016-05-18 17:38:39 +0800

    33

---
+++ obj/local/armeabi/objs/speex/libspeex/kiss_fftr.o.d
@@ -0,0 +1,117 @@
+obj/local/armeabi/objs/speex/libspeex/kiss_fftr.o: \
+ jni/libspeex/kiss_fftr.c jni/libspeex/os_support.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/string.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/android/api-level.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/malloc.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/stdio.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/types.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/stdint.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/_types.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/machine/_types.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/posix_types.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/stddef.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/compiler.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/asm/posix_types.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/asm/types.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/types.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/machine/kernel.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/stdlib.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/alloca.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/strings.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/memory.h \
+ jni/libspeex/kiss_fftr.h jni/libspeex/kiss_fft.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/math.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/limits.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/limits.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/limits.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/machine/internal_types.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/machine/limits.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/syslimits.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/asm/page.h \
+ jni/libspeex/arch.h jni/include/speex/speex_types.h \
+ jni/include/speex/speex_config_types.h jni/libspeex/fixed_generic.h \
+ jni/libspeex/_kiss_fft_guts.h jni/libspeex/math_approx.h
+
+jni/libspeex/os_support.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/string.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/android/api-level.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/malloc.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/stdio.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/types.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/stdint.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/_types.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/machine/_types.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/posix_types.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/stddef.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/compiler.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/asm/posix_types.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/asm/types.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/types.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/machine/kernel.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/stdlib.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/alloca.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/strings.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/memory.h:
+
+jni/libspeex/kiss_fftr.h:
+
+jni/libspeex/kiss_fft.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/math.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/limits.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/limits.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/limits.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/machine/internal_types.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/machine/limits.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/syslimits.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/asm/page.h:
+
+jni/libspeex/arch.h:
+
+jni/include/speex/speex_types.h:
+
+jni/include/speex/speex_config_types.h:
+
+jni/libspeex/fixed_generic.h:
+
+jni/libspeex/_kiss_fft_guts.h:
+
+jni/libspeex/math_approx.h:
---
+++ obj/local/armeabi/objs/speex/libspeex/vbr.o.d
@@ -0,0 +1,85 @@
+obj/local/armeabi/objs/speex/libspeex/vbr.o: jni/libspeex/vbr.c \
+ jni/libspeex/vbr.h jni/libspeex/arch.h jni/include/speex/speex_types.h \
+ jni/include/speex/speex_config_types.h jni/libspeex/fixed_generic.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/math.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/android/api-level.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/types.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/stdint.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/_types.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/machine/_types.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/posix_types.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/stddef.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/compiler.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/asm/posix_types.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/asm/types.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/types.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/machine/kernel.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/limits.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/limits.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/limits.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/machine/internal_types.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/machine/limits.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/syslimits.h \
+ E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/asm/page.h
+
+jni/libspeex/vbr.h:
+
+jni/libspeex/arch.h:
+
+jni/include/speex/speex_types.h:
+
+jni/include/speex/speex_config_types.h:
+
+jni/libspeex/fixed_generic.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/math.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/android/api-level.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/types.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/stdint.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/_types.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/machine/_types.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/posix_types.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/stddef.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/compiler.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/asm/posix_types.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/asm/types.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/types.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/machine/kernel.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/limits.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/limits.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/limits.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/machine/internal_types.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/machine/limits.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/syslimits.h:
+
+E:/WorkSpace/android-ndk-r10/platforms/android-19/arch-arm/usr/include/asm/page.h:
